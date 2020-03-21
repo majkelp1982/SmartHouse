9808eaa6a37cea438829a482ae52d6fc7a7aff9d
majkelp1982 <s.pomazanka@gmail.com>
2020-03-21 21:07:50 +0100

    Security added

--- src/main/java/pl/pomazanka/SmartHouse/SmartHouseApplication.java
+++ src/main/java/pl/pomazanka/SmartHouse/SmartHouseApplication.java
@@ -2,8 +2,10 @@ package pl.pomazanka.SmartHouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class SmartHouseApplication {
 
 	public static void main(String[] args) {
