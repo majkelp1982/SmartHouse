90824b9ad494657c9889be066a8ff5d98bc31b89
majkelp1982 <s.pomazanka@gmail.com>
2022-03-05 08:23:39 +0100

    Solar Panels wip

--- src/test/java/pl/pomazanka/SmartHouse/SmartHouseApplicationTests.java
+++ src/test/java/pl/pomazanka/SmartHouse/SmartHouseApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SmartHouseApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
--- src/test/java/pl/pomazanka/SmartHouse/test.java
+++ src/test/java/pl/pomazanka/SmartHouse/test.java
@@ -2,7 +2,7 @@ package pl.pomazanka.SmartHouse;
 
 public class test {
 
-	public static void main(String[] args) {
-		System.out.println("Start");
-	}
+  public static void main(String[] args) {
+    System.out.println("Start");
+  }
 }
